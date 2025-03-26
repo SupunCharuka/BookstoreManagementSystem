0aad58b098d80bde58add114c1dc9cdbe91a7001
SupunCharuka <supuncharuka.dev@gmail.com>
2025-03-26 14:27:32 +0530

    Change startup form to Splash and add timer functionality
    Modified Program.cs to set Splash as the startup form. Updated Splash.Designer.cs to add components, replace label2 with lblStatus, add a timer, and handle the Load event. In Splash.cs, added using directive, implemented Splash_Load to start the timer, and timer1_Tick to update UI and transition to Login form. Added timer metadata in Splash.resx.

--- Spalsh.Designer.cs
+++ Spalsh.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spalsh));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,15 +67,19 @@
             this.progressBar1.Size = new System.Drawing.Size(608, 17);
             this.progressBar1.TabIndex = 2;
             // 
-            // label2
+            // lblStatus
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(7, 300);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Loading...";
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(7, 300);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(97, 25);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Loading...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Spalsh
             // 
@@ -81,7 +87,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(632, 357);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -89,6 +95,7 @@
             this.Name = "Spalsh";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Spalsh_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -100,7 +107,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Spalsh.cs
+++ Spalsh.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace BookstoreManagementSystem
 {
@@ -20,6 +21,43 @@ namespace BookstoreManagementSystem
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Spalsh_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(5);
+
+            // Update status text based on progress
+            if (progressBar1.Value < 30)
+            {
+                lblStatus.Text = "Loading resources...";
+            }
+            else if (progressBar1.Value < 70)
+            {
+                lblStatus.Text = "Connecting to database...";
+            }
+            else
+            {
+                lblStatus.Text = "Starting application...";
+            }
+
+            // When complete
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                timer1.Stop();
+                this.Hide();
+
+                Login login = new Login();
+                login.Show();
+
+           
+            }
         }
     }
 }
